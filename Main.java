4d031a5b7b476f50d67f2ed4985f432307ca6f9e
nitecifer <nitegupta2004@gmail.com>
2025-01-31 23:01:30 +0530

    list

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(2,25);
+      
+        System.out.println(list);
+    }
+}
+
